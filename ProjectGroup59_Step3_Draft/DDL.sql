50960b10ef16e774de48477196b70685f543312d
Mike <clancymi@oregonstate.edu>
2023-02-16 15:53:23 -0500

    Fixes links to edit pages

---
+++ ProjectGroup59_Step3_Draft/DDL.sql
@@ -0,0 +1,95 @@
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
+
+CREATE OR REPLACE TABLE Class_Categories
+(
+  class_category_id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(45) NOT NULL,
+  PRIMARY KEY (class_category_id)
+);
+
+CREATE OR REPLACE TABLE Classes 
+(
+  class_id int(11) NOT NULL AUTO_INCREMENT,
+  professor_id int(11) NOT NULL,
+  class_category_id int(11) NOT NULL,
+  name varchar(145) NOT NULL,
+  current_enrollment int(11) NOT NULL,
+  max_enrollment int(11) NOT NULL,
+  PRIMARY KEY (class_id),
+  KEY `fk_Classes_Professors1_idx` (professor_id),
+  KEY `fk_Classes_Class Categories1_idx` (class_category_id),
+  CONSTRAINT `fk_Classes_Class Categories1` FOREIGN KEY (class_category_id) REFERENCES Class_Categories (class_category_id) ON DELETE CASCADE,
+  CONSTRAINT `fk_Classes_Professors1` FOREIGN KEY (professor_id) REFERENCES Professors (professor_id) ON DELETE CASCADE
+);
+
+
+CREATE OR REPLACE TABLE Professors 
+(
+  professor_id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(145) NOT NULL,
+  street_address varchar(145) NOT NULL,
+  city varchar(45) NOT NULL,
+  state varchar(45) NOT NULL,
+  zip_code varchar(45) NOT NULL,
+  email varchar(45) NOT NULL,
+  PRIMARY KEY (professor_id)
+);
+
+CREATE OR REPLACE TABLE Registrations 
+(
+  registration_id int(11) NOT NULL AUTO_INCREMENT,
+  student_id int(11) NOT NULL,
+  class_id int(11) NOT NULL,
+  date_time_of_registration date NOT NULL,
+  semester varchar(45) NOT NULL,
+  PRIMARY KEY (registration_id),
+  KEY `fk_Registrations_Students_idx` (student_id),
+  KEY `fk_Registrations_Classes1_idx` (class_id),
+  CONSTRAINT `fk_Registrations_Classes1` FOREIGN KEY (class_id) REFERENCES Classes (class_id) ON DELETE CASCADE,
+  CONSTRAINT `fk_Registrations_Students` FOREIGN KEY (student_id) REFERENCES Students (student_id) ON DELETE CASCADE
+);
+
+CREATE OR REPLACE TABLE Students
+(
+  student_id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(145) NOT NULL,
+  street_address varchar(145) NOT NULL,
+  city varchar(45) NOT NULL,
+  state varchar(45) NOT NULL,
+  zip_code varchar(45) NOT NULL,
+  email varchar(45) NOT NULL,
+  phone_number varchar(45) NOT NULL,
+  PRIMARY KEY (student_id)
+);
+
+
+INSERT INTO Class_Categories (name)
+VALUES ('Psychology'),
+('Mathematics'),
+('Computer Science'),
+('Nursing');
+
+INSERT INTO Classes (professor_id, class_category_id, name, current_enrollment, max_enrollment)
+VALUES (1,1,'Psychology 101',0,30),
+(1,1,'Psychology 201',0,30),
+(2,2,'Intro to Geometry',0,100);
+
+INSERT INTO Professors (name, street_address, city, state, zip_code, email)
+VALUES ('Marito Viana','1 first st.','Yakima','TX','72556','marito.viana@example.com'),
+('Andy Martin','2 second st.','Fullerton','WV','63361','andy.martin@example.com'),
+('Wendy Miller','3 third st.','Houston','MI','46194','wendy.miller@example.com');
+
+INSERT INTO Registrations (student_id, class_id, date_time_of_registration, semester) 
+VALUES (1,2,'2023-03-02','SUMMER 2023'),
+(2,3,'2023-02-04','SUMMER 2023'),
+(2,1,'2023-01-25','SUMMER 2023');
+
+INSERT INTO Students (name, street_address, city, state, zip_code, email, phone_number)
+VALUES ('Travis Bell','1 hello st','Chandler','NY','13941','travis.bell@example.com','(576) 254-3812'),
+('Caroline Lynch','2 bye st.','New York','LA','35397','caroline.lynch@example.com','(903) 425-6641'),
+('Ashley Pena','3 why st.','Arvada','RI','86584','ashley.pena@example.com','(508) 464-0886');
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
---
+++ ProjectGroup59_Step3_Draft/DML.sql
@@ -0,0 +1,164 @@
+-- The following are our data manipulation queries. We are using a colon (":") character to 
+-- denote the variables that will have data from the backend programming language
+
+---------------------------------------READ---------------------------------------------
+-- display all students from Students table
+SELECT student_id, name AS student_name, street_address,
+city, state, zip_code, email, phone_number
+FROM Students;
+
+-- display all classes from Classes table
+SELECT Classes.class_id, Professors.name AS professor_name, `Class Categories`.name AS class_category_name, 
+Classes.name AS class_name, Classes.current_enrollment, Classes.max_enrollment
+FROM Classes
+    INNER JOIN Professors
+    ON Classes.professor_id = Professors.professor_id
+    INNER JOIN `Class Categories`
+    ON Classes.class_category_id = `Class Categories`.class_category_id;
+
+-- display all registrations from Registration table
+SELECT Registrations.registration_id, Students.name AS student_name, Classes.name AS class_name, 
+Registrations.date_time_of_registration, Registrations.semester
+FROM Registrations
+    INNER JOIN Students
+    ON Registrations.student_id = Students.student_id
+    INNER JOIN Classes
+    ON Registrations.class_id = Classes.class_id;
+
+-- display all class categories from Class Categories table
+SELECT class_category_id, name AS class_category_name
+FROM `Class Categories`;
+
+-- display all professors from the Professors table
+SELECT professor_id, name AS professor_name, street_address,
+city, state, zip_code, email
+FROM Professors;
+
+------------------------------------------CREATE-------------------------------------------
+
+
+-- Insert Into the Students Table
+INSERT INTO Students (name, street_address, city, state, zip_code, email, phone_number)
+VALUES(:studentNameInput,:streeAddessInput,:cityInput, :stateInput, :zip_codeInput, :emailInput, :phoneNumberInput);
+
+
+-- get all professor ids and names to populate the professorId_dropdown input for inserting into the Classes Table
+SELECT professor_id, name
+FROM Professors;
+
+-- get all class category ids and names to populate the classCategoryID_dropdown input for inserting into the Classes Table 
+SELECT class_category_id, name
+FROM `Class Categories`;
+
+-- Insert into the Classes Table
+INSERT INTO Classes (professor_id, class_category_id, name, current_enrollment, max_enrollment)
+VALUES(:professorId_dropdown_input, :classCategoryID_dropdown_input, :classNameInput, :currentEnrollmentInput, :maxEnrollmentInput);
+
+-- get all student ids and names to populate the studentId_dropdown_input for inserting into the Registrations Table
+SELECT student_id, name
+FROM Students;
+
+-- get all class ids and names to populate the classId_dropdown_input for inserting into the Registrations Table
+SELECT class_id, name
+FROM Classes;
+
+-- Insert into the Registrations Table
+INSERT INTO Registrations (student_id, class_id, date_time_of_registration, semester)
+VALUES(:studentId_dropdown_input, :classId_dropdown_input, :dateInput, :semesterInput);
+
+-- Insert into the Class Categories Table
+INSERT INTO `Class Categories` (name)
+VALUES(:classCategoryNameInput);
+
+-- Insert into the Professors Table
+INSERT INTO Professors (name, street_address, city, state, zip_code, email)
+VALUES(:professorNameInput, :streetAddressInput, :cityInput, :stateInput, :zipCodeInput, :emailInput);
+
+
+
+-- ================== UPDATE Queries =======================
+
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
+
+-- Update One Entry in Class Categories Table
+
+UPDATE `Class Categories`
+SET name = :classCategoryNameInput
+WHERE class_category_id = :class_category_id;
+
+-- Update One Entry in Student Table
+
+UPDATE Students
+SET name=:studentNameInput, 
+street_address= :streeAddessInput,
+city=:cityInput, 
+state=:stateInput, 
+zip_code = :zip_codeInput, 
+email = :emailInput, 
+phone_number= :phoneNumberInput
+WHERE student_id = :student_id;
+
+-- Update One Entry in Classes Table
+
+UPDATE Classes
+SET professor_id = :professorId_dropdown_input, 
+class_category_id = :classCategoryID_dropdown_input, 
+name = :classNameInput, 
+current_enrollment = :currentEnrollmentInput, 
+max_enrollment = :maxEnrollmentInput
+WHERE class_id = :class_id;
+
+-- Update One Entry in Professors Table
+
+UPDATE Professors
+SET name = :professorNameInput, 
+street_address = :streetAddressInput, 
+city = :cityInput, 
+state = :stateInput, 
+zip_code = :zipCodeInput, 
+email = :emailInput
+WHERE professor_id = :professor_id;
+
+-- =======================DELETE Queries===============
+
+-- Delete one entry from Students table
+
+DELETE FROM Students WHERE student_id = :student_id;
+
+-- Delete all entries from Students table
+
+DELETE FROM Students;
+
+-- Delete one entry from Class Categories table
+
+DELETE FROM `Class Categories` WHERE class_category_id = :class_category_id;
+
+-- Delete all entries from Class Categories table
+
+DELETE FROM `Class Categories`;
+
+-- Delete one entry from Classes table
+
+DELETE FROM Classes WHERE class_id = :class_id;
+
+-- Delete all entries from Classes table
+
+DELETE FROM Classes;
+
+-- Delete one entry from Professors table
+
+DELETE FROM Professors WHERE professor_id = :professor_id;
+
+-- Delete all entries from Professors table
+
+DELETE FROM Professors;
+
+-- Delete one entry from Registrations table
+
+DELETE FROM Registrations WHERE registration_id = :registration_id;
+
+-- Delete all entries from Registrations table
+
+DELETE FROM Registrations;
