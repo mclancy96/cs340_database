60a72999971ea77974c1139f4571232d27ae4ede
Mike <clancymi@oregonstate.edu>
2023-02-24 17:51:11 -0500

    CRUD Complete for students

--- routes/student_routes.js
+++ routes/student_routes.js
@@ -4,15 +4,58 @@
 const express = require('express');
 const router = express.Router();
 const students = require('../services/students');
+router.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
+// Read all
 router.get('/students', async function (req, res) {
-    let studentResults = await students.getStudents();
-    res.render("students/students");
+    const studentResults = await students.getStudents();
+    res.render("students/students", { students: studentResults });
 });
 
-router.get('/students/:id/edit', function (req, res) {
-    res.render("students/edit_students");
+// Create
+router.post('/students/create', async function (req, res) {
+    try {
+        await students.createStudent(req.body);
+        res.redirect("/students");
+    }
+    catch (err) {
+        console.log("Error creating student: ", err);
+        res.redirect('/students');
+    }
 });
 
+// Read One
+router.get('/students/:id/edit', async function (req, res) {
+    const studentResult = await students.getStudentById(req.params.id);
+    res.render("students/edit_students", { student: studentResult[0] });
+});
+
+// Update
+router.post('/students/:id/edit', async function (req, res) {
+    try {
+        await students.updateStudent(req.params.id, req.body);
+        res.redirect("/students");
+    }
+    catch (err) {
+        console.log("Error updating student: ", err);
+        res.redirect('/students');
+    }
+});
+
+// Delete
+router.post('/students/:id/delete', async function (req, res) {
+    try {
+        await students.deleteStudent(req.params.id);
+        res.redirect("/students");
+    }
+    catch (err) {
+        console.log("Error deleting student: ", err);
+        res.redirect('/students');
+    }
+});
 
 module.exports = router;
--- services/students.js
+++ services/students.js
@@ -8,10 +8,56 @@ async function getStudents() {
     const students = await db.query(
         `SELECT * FROM Students`
     );
-
     return students;
 }
 
+async function getStudentById(studentId) {
+    const student = await db.query(
+        `SELECT * FROM Students WHERE student_id = ${studentId}`
+    );
+    return student;
+}
+
+async function createStudent(studentObject) {
+    const updateStatus = await db.query(
+        `INSERT INTO Students (name, street_address, city, state, zip_code, email, phone_number)
+        VALUES("${studentObject.name}", 
+        "${studentObject.street_address}",
+        "${studentObject.city}", 
+        "${studentObject.state}", 
+        "${studentObject.zip_code}", 
+        "${studentObject.email}", 
+        "${studentObject.phone_number}");`
+    );
+    return updateStatus;
+}
+
+async function updateStudent(studentId, studentObject) {
+    const updateStatus = await db.query(
+        `UPDATE Students
+        SET name = "${studentObject.name}", 
+        street_address= "${studentObject.street_address}",
+        city="${studentObject.city}", 
+        state="${studentObject.state}", 
+        zip_code = "${studentObject.zip_code}", 
+        email = "${studentObject.email}", 
+        phone_number= "${studentObject.phone_number}"
+        WHERE student_id = "${studentId}";`
+    );
+    return updateStatus;
+}
+
+async function deleteStudent(studentId) {
+    const updateStatus = await db.query(
+        `DELETE FROM Students WHERE student_id = "${studentId}";`
+    );
+    return updateStatus;
+}
+
 module.exports = {
-    getStudents
+    getStudents,
+    getStudentById,
+    createStudent,
+    updateStudent,
+    deleteStudent
 }
