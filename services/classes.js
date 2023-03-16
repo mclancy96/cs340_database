b689d13507a2cf4af12ccbff5aedd12d50b87667
Mike <clancymi@oregonstate.edu>
2023-03-15 20:38:41 -0600

    Changes way information is added to ejs

--- services/classes.js
+++ services/classes.js
@@ -6,16 +6,7 @@ const db = require('./db');
 
 async function getClasses() {
     const classes = await db.query(
-        `SELECT Classes.class_id, Professors.professor_id AS professor_id, Professors.name AS professor_name, Class_Categories.class_category_id AS category_id, Class_Categories.name AS class_category_name, 
-            Classes.name AS class_name, COUNT(Registrations.class_id) AS current_enrollment, Classes.max_enrollment
-        FROM Classes
-            INNER JOIN Professors
-            ON Classes.professor_id = Professors.professor_id
-            INNER JOIN Class_Categories
-            ON Classes.class_category_id = Class_Categories.class_category_id
-            LEFT JOIN Registrations
-            ON Classes.class_id = Registrations.class_id
-        GROUP BY Classes.class_id`
+        `SELECT * FROM Classes;`
     );
 
     return classes;
@@ -23,33 +14,56 @@ async function getClasses() {
 
 async function getClassById(classId) {
     const classes = await db.query(
-        `SELECT * FROM Classes WHERE class_id = ${classId}`
+        `SELECT * FROM Classes WHERE class_id = ${classId};`
     );
     return classes;
 }
 
 async function createClass(classObject) {
-    const updateStatus = await db.query(
-        `INSERT INTO Classes (name, professor_id, class_category_id, current_enrollment, max_enrollment)
+    if (classObject.professor_id == "None") {
+        const updateStatus = await db.query(
+            `INSERT INTO Classes (name, class_category_id, current_enrollment, max_enrollment)
+        VALUES("${classObject.name}", 
+        "${classObject.class_category_id}", 
+        "0", 
+        "${classObject.max_enrollment}");`
+        );
+        return updateStatus;
+    } else {
+        const updateStatus = await db.query(
+            `INSERT INTO Classes (name, professor_id, class_category_id, current_enrollment, max_enrollment)
         VALUES("${classObject.name}", 
         "${classObject.professor_id}",
         "${classObject.class_category_id}", 
         "0", 
         "${classObject.max_enrollment}");`
-    );
-    return updateStatus;
+        );
+        return updateStatus;
+    }
+
 }
 
 async function updateClass(classId, classObject) {
-    const updateStatus = await db.query(
-        `UPDATE Classes
+    if (classObject.professor_id == "None") {
+        const updateStatus = await db.query(
+            `UPDATE Classes
+            SET name = "${classObject.name}", 
+            class_category_id="${classObject.class_category_id}", 
+            max_enrollment = "${classObject.max_enrollment}"
+            WHERE class_id = "${classId}";`
+        );
+        return updateStatus;
+    } else {
+        const updateStatus = await db.query(
+            `UPDATE Classes
         SET name = "${classObject.name}", 
         professor_id= "${classObject.professor_id}",
         class_category_id="${classObject.class_category_id}", 
         max_enrollment = "${classObject.max_enrollment}"
         WHERE class_id = "${classId}";`
-    );
-    return updateStatus;
+        );
+        return updateStatus;
+    }
 }
 
 async function deleteClass(classId) {
